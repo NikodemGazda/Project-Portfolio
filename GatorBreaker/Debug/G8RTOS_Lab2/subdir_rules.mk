1aac8ebd52f7f795cf4e40adec46987d97e5272f
NikodemGazda <nikigazda@gmail.com>
2023-03-12 00:18:57 -0500

    Add files via upload

---
+++ GatorBreaker/Debug/G8RTOS_Lab2/subdir_rules.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+G8RTOS_Lab2/%.obj: ../G8RTOS_Lab2/%.s $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: Arm Compiler'
+	"C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --opt_for_speed=0 --include_path="C:/Users/Nikodem/OneDrive - University of Florida/fall 22/up2/labs/final" --include_path="C:/Users/Nikodem/OneDrive - University of Florida/fall 22/up2/labs/lab 4/lab/BoardSupport" --include_path="C:/Users/Nikodem/OneDrive - University of Florida/fall 22/up2/labs/lab 4/lab/G8RTOS_Lab2" --include_path="C:/ti/TivaWare_C_Series-2.2.0.295" --include_path="C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include" --include_path="C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="G8RTOS_Lab2/$(basename $(<F)).d_raw" --obj_directory="G8RTOS_Lab2" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+G8RTOS_Lab2/%.obj: ../G8RTOS_Lab2/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: Arm Compiler'
+	"C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --opt_for_speed=0 --include_path="C:/Users/Nikodem/OneDrive - University of Florida/fall 22/up2/labs/final" --include_path="C:/Users/Nikodem/OneDrive - University of Florida/fall 22/up2/labs/lab 4/lab/BoardSupport" --include_path="C:/Users/Nikodem/OneDrive - University of Florida/fall 22/up2/labs/lab 4/lab/G8RTOS_Lab2" --include_path="C:/ti/TivaWare_C_Series-2.2.0.295" --include_path="C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include" --include_path="C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="G8RTOS_Lab2/$(basename $(<F)).d_raw" --obj_directory="G8RTOS_Lab2" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
